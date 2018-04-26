cd9bcad980ce556271f8dfa68a0d6a93c8028b40
Mostafa Medht Abdellah <mostafamedht98@gmail.com>
2018-04-26 22:45:28 +0200

    adding ecommerce templte and making product facory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -19,5 +20,19 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    $name = $faker->sentence(5);
+    $slug = str_slug($name);
+    return [
+        
+        'name' => $name,
+        'image' => 'uploads/products/book1.png',
+        'description' => $faker->paragraph(4),
+        'price' => $faker->numberBetween(100,10000),
+        'slug' => $slug
     ];
 });
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,21 +11,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        $p1= [
-            'name' => 'Learn to build website in HTML5',
-            'image' => 'uploads/products/book.png',
-            'price' => 5000,
-            'description' => 'Loream ipsum dolor sit amet,Loream ipsum dolor sit amet,Loream ipsum dolor sit amet.'
-        ];
-
-        $p2= [
-            'name' => 'PHP Development in depth',
-            'image' => 'uploads/products/book.png',
-            'price' => 2400,
-            'description' => 'Loream ipsum dolor sit amet,Loream ipsum dolor sit amet,Loream ipsum dolor sit amet.'
-        ];
-
-        Product::create($p1);
-        Product::create($p2);
+        factory(App\Product::class,30)->create();
     }
 }
